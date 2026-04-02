4e918a102316bb80be2ef57dce1b0cccee831c05
Shuvechhya Bajracharya <shuvechhyabajracharya@Shuvechhyas-MacBook-Air.local>
2026-04-02 11:14:06 +0700

    updated docker

--- main.py
+++ main.py
@@ -875,4 +875,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    PORT = int(os.getenv("PORT", "3000"))
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
